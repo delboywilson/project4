0295f65abb72001f090b71b3e2d5871d8613b013
delboywilson <75227946+delboywilson@users.noreply.github.com>
2021-02-23 12:44:50 +1000

    Add homepage route and render all user schedules in db on homepage

---
+++ routes/homepage.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db/database')
+
+// router.get('/', (req, res) => {
+//   res.render('pages/homepage')
+// })
+
+router.get('/', (req, res) => {
+  db.any('SELECT * from schedules;')
+    .then((schedule) => {
+      console.log(schedule)
+      res.render('pages/homepage', {
+        mySchedule: schedule
+      })
+    })
+    .catch((err) => {
+      console.error(err)
+      res.render('pages/error2', {
+        err: err
+      })
+    })
+})
+
+// router.post('/login', (req, res) => {
+//   const hashSymbols = crypto.createHash('sha256').update(req.body.password).digest('hex')
+//   const newSchedule = {
+//     email: req.body.email,
+//     password: hashSymbols
+//   }
+//   // TODO work on password db
+//   db.schedules.push(newSchedule)
+//   res.redirect('/homepage')
+// })
+
+module.exports = router
