f39df7bf4996b9a9dddb98d0409106d802c7c105
delboywilson <75227946+delboywilson@users.noreply.github.com>
2021-03-02 19:40:33 +1000

    Add new query to signup route

--- routes/signup.js
+++ routes/signup.js
@@ -39,17 +39,22 @@ router.post('/', (req, res) => {
 
 const { userID } = req.session
 
-// TODO client side verification here
-
 let v1 = req.body.last_name
 let v2 = req.body.first_name
 let v3 = req.body.email
 let v4 = bcrypt.hashSync(req.body.password, 10)
 
-// TODO need to check first if email exists and don't add if they do
+// TODO need to add more validation here
+// TODO cookie is not being created for signup
 
 db.none('INSERT INTO users(last_name, first_name, email, password) VALUES ($1, $2, $3, $4);', [v1, v2, v3, v4])
  .then(() => {
+  db.query('SELECT * FROM users WHERE email = $1;', [v3]) 
+  .then((results) => {
+    const newID = results[0]
+    console.log(newID.user_id)
+    req.session.userID = newID.user_id
+  })
     res.redirect('/homepage')
   })
   .catch((err) => {
@@ -61,14 +66,3 @@ db.none('INSERT INTO users(last_name, first_name, email, password) VALUES ($1, $
  })
 
 module.exports = router
-
-
-// req.session.userID = 
-// new user_id that was just created
-
-// db.query('SELECT * FROM users WHERE email = $1;', [v3]) 
-//   .then((results) => {
-//     const newID = results[0]
-//     console.log(newID.user_id)
-//     req.session.userID = newID.user_id
-//   })
